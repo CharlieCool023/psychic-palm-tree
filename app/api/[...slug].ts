48c0c9dbff746af9a490fd834c4d60f261b06a10
charliecool023 <charlesonah023@gmail.com>
2026-04-29 14:53:56 +0100

    fixes

--- app/api/[...slug].ts
+++ app/api/[...slug].ts
@@ -1,6 +1,5 @@
+import { handle } from 'hono/vercel';
 import app from '../src/api/boot';
 
-// Vercel Serverless Function using Fetch API
-export default async (req: Request): Promise<Response> => {
-  return app.fetch(req);
-};
+// Vercel Serverless Function
+export default handle(app);
