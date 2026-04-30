7f4905830d4b8191863c4c70349ec49d4b998b9c
charliecool023 <charlesonah023@gmail.com>
2026-04-30 03:50:24 +0100

    update

--- app/api/[...slug].ts
+++ app/api/[...slug].ts
@@ -1,4 +1,9 @@
-import { handle } from 'hono/vercel';
-import app from '../dist/api.js';
+import app from '../src/api/boot';
 
-export default handle(app);
+export const config = {
+  runtime: 'edge',
+};
+
+export default async function (req: Request) {
+  return app.fetch(req);
+}
